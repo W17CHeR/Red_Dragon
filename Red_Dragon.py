236b9573d786bf411571fccf4ce467510d6ea46b
W17CH3R <draak18@protonmail.com>
2025-05-26 13:32:54 +0200

    Update Red_Dragon.py

--- Red_Dragon.py
+++ Red_Dragon.py
@@ -47,6 +47,9 @@ def print_title():
 """
     print(colored(ascii_logo, 'red'))
     print(colored(os.popen('figlet -f slant "Red Dragon"').read(), 'red'))
+    print(colored("Desarrollado por Sergio aka: W17CHeR", 'green'))
+    print(colored("Este programa es para fines educativos y de entretenimiento, no se debe usar para actividades ilegales.", 'yellow'))
+    print(colored("Version 1.0.1", 'blue'))
 
 # Aquí mostrara el menú
 
